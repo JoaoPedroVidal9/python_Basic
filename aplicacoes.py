cf05288bffb2f6ca68ac5e8b9abf70b1d7aa9f25
JoaoPedroVidal9 <jp.joaopedro.franc0@gmail.com>
2024-06-03 13:15:42 -0300

    atualização final do aplicações.py

--- aplicacoes.py
+++ aplicacoes.py
@@ -11,7 +11,7 @@ for i in lista:
         mNum = i
 
 print("O maior número da lista é:", mNum)
-
+print("---------------------------------------------")
 #Exemplo2
 
 mylist = [rd(0, 20) for i in range(10)]
@@ -23,7 +23,7 @@ for i in range(len(mylist)):
         maior = mylist[i]
 
 print("O maior número da lista 2 é:", maior)
-
+print("---------------------------------------------")
 #Exemplo3:
 
 uL = mylist[:]
@@ -32,7 +32,7 @@ for i in uL[1:]:
     if i > mel:
         mel = i
 print("O maior número da lista 3 é:", mel)
-
+print("---------------------------------------------")
 #Encontrar a localização de um determinado elemento dentro de uma lista
 frutas = ["abacaxi", "maçã", "pêra", "mamão", "uva", "melancia"]
 elemento = "melancia"
@@ -47,5 +47,39 @@ if achado:
     print("Achou! Índice", i)
 else:
     print("nem...")
+
+print("---------------------------------------------")
+#Conferidor de jogos de loteria:
+sorteados = [5, 11, 9, 42, 3, 49]
+apostados = [3, 7, 11, 42, 34, 49]
+acertos = 0
+
+for num in sorteados:
+    if num in apostados:
+        acertos += 1
+
+print(acertos)
+print("---------------------------------------------")
+
+#Remoção de números repetidos em uma lista
+lista = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
+
+print("Lista original:", lista)
+
+#lista de apoio
+vistos = []
+
+#Iterar pela lista original de trás pra frente
+for i in range(len(lista) -1, -1, -1):
+    #Se o número já está na lista vistos, removê-lo da lista original
+    if lista[i] in vistos:
+        del lista[i]
+    #Senão, adciona ao final da lista vistos
+    else:
+        vistos.append(lista[i])
+    print("Teste:", i)
+
+#Exibo a lista modificada
+print("Lista modificada:", lista)
 
 tp.sleep(1)
