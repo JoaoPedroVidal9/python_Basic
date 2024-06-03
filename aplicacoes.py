a7184f4f5a7c0802b071ffd6af44bba9efbc109e
JoaoPedroVidal9 <jp.joaopedro.franc0@gmail.com>
2024-06-03 11:32:48 -0300

    atualização do aplicações.py

--- aplicacoes.py
+++ aplicacoes.py
@@ -12,10 +12,40 @@ for i in lista:
 
 print("O maior número da lista é:", mNum)
 
+#Exemplo2
 
+mylist = [rd(0, 20) for i in range(10)]
+print(mylist)
+maior = mylist[0]
 
+for i in range(len(mylist)):
+    if mylist[i] > maior:
+        maior = mylist[i]
 
+print("O maior número da lista 2 é:", maior)
 
+#Exemplo3:
 
+uL = mylist[:]
+mel = uL[0]
+for i in uL[1:]:
+    if i > mel:
+        mel = i
+print("O maior número da lista 3 é:", mel)
+
+#Encontrar a localização de um determinado elemento dentro de uma lista
+frutas = ["abacaxi", "maçã", "pêra", "mamão", "uva", "melancia"]
+elemento = "melancia"
+achado = False
+
+for i in range(len(frutas)):
+    achado = frutas[i] == elemento
+    if achado:
+        break
+
+if achado:
+    print("Achou! Índice", i)
+else:
+    print("nem...")
 
 tp.sleep(1)
