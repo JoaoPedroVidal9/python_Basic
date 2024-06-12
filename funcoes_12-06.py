a56169a6d786c0013e606571e42b687c6986d3d6
JoaoPedroVidal9 <jp.joaopedro.franc0@gmail.com>
2024-06-12 10:51:41 -0300

    commit certo 12-06

--- funcoes_12-06.py
+++ funcoes_12-06.py
@@ -1,38 +1,37 @@
 import time
 
-# def hi_everybody(lista):
-#     for name in lista:
-#         print("Oi", name)
+def hi_everybody(lista):
+    for name in lista:
+        print("Oi", name)
 
-# hi_everybody(["Tina", "Gabi", "Jão"])
+hi_everybody(["Tina", "Gabi", "Jão"])
 
-# def criar_lista(n):
-#     lista = []
-#     for i in range(n):
-#         lista.append(i)
-#     return lista
+def criar_lista(n):
+    lista = []
+    for i in range(n):
+        lista.append(i)
+    return lista
  
-# print(criar_lista(10))
+print(criar_lista(10))
 
-# def list_updater(lst):
-#     upd_list = []
-#     for elem in lst:
-#         elem **= 2
-#         upd_list.append(elem)
-#     return upd_list
+def list_updater(lst):
+    upd_list = []
+    for elem in lst:
+        elem **= 2
+        upd_list.append(elem)
+    return upd_list
  
-# foo = [1, 2, 3, 4, 5]
-# print(list_updater(foo))
+foo = [1, 2, 3, 4, 5]
+print(list_updater(foo))
 
-# global a
-# a = 1
-# def fun():
-#     global a
-#     a = 2
-#     print(a)
+global a
+a = 1
+def fun():
+    a = 2
+    print(a)
  
-# fun()
-# print(a)
+fun()
+print(a)
 
 
 
